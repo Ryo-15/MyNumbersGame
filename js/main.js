c8a7e4dbf6a5df4a4322789c80ba9dadcfdba524
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-24 19:51:28 +0900

    [finish]numbers_js_lesson_8

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+
+    getEL() {
+      return this.el;
+    }
   }
   class Board {
     constructor() {
@@ -13,6 +17,15 @@
       for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel());
       }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        // board.appendChild(panel.el);
+        board.appendChild(panel.getEL());
+      });
     }
   }
 
